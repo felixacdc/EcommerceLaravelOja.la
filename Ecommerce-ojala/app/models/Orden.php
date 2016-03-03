a6644bd59d806854529a00aa27aeac3e27e5036c
felixacdc <pfmata01@gmail.com>
2016-03-02 23:11:14 -0600

    12. - Tabla pivote

--- Ecommerce-ojala/app/models/Orden.php
+++ Ecommerce-ojala/app/models/Orden.php
@@ -1,7 +1,26 @@
 <?php
 
 class Orden extends Eloquent {
+	//propiedades que el usuario no puede modificar
 	protected $guarded = array();
 
 	public static $rules = array();
+
+	//propiedades que el usuario puede modificar
+	protected $filelable = ['miembro_id', 'direccion', 'total', 'estado'];
+
+	// definir tabla pivote
+	public function ordenItems ()
+	{
+		// Retornar la relacion 
+		// primer dato el modelo
+		// segundo parametro nombre de la clase pivote
+		// tercer parametro llave foranea de la primera tabla
+		// cuarto parametro llave foranea de la segunda tabla
+		// con withPivot se definen los campos de retorno de la tabla pivote en este caso ordenlibros
+		return $this->belongsToMany('Libro', 'ordenlibros', 'orden_id', 'libro_id')->withPivot('cantidad', 'precio', 'total');
+
+	}
+
+
 }
